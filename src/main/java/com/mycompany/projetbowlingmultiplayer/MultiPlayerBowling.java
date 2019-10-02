e43203130249488e90897456b43b6d6c41c65865
pedago <pedago@pcalb-mm0419.univ-jfc.fr>
2019-10-02 14:37:28 +0200

    Fin de tp n°2

--- src/main/java/com/mycompany/projetbowlingmultiplayer/MultiPlayerBowling.java
+++ src/main/java/com/mycompany/projetbowlingmultiplayer/MultiPlayerBowling.java
@@ -1,94 +1,89 @@
 package com.mycompany.projetbowlingmultiplayer;
 
 import bowling.MultiPlayerGame;
- import bowling.SinglePlayerGame;
+import bowling.SinglePlayerGame;
+import java.util.ArrayList;
 import java.util.HashMap;
 
-/**
- *
- * @author pedago
- */
+
 public class MultiPlayerBowling implements MultiPlayerGame {
 
-    private boolean GameEnCours;
-    
-    public HashMap game = new HashMap<>();
-    public SinglePlayerGame GameActuelle;
+    private boolean jeuencours;
 
-    
+    public HashMap<String, SinglePlayerGame> jeu;
+    public SinglePlayerGame jeuactuel;
+
+    private ArrayList<String> listejoueur;
+    private String joueuractuel;
+
     public MultiPlayerBowling() {
-        HashMap game = new HashMap<>();
+        HashMap jeu = new HashMap<>();
     }
-    
-    
+
     @Override
     public String startNewGame(String[] playerName) throws Exception {
-        if(playerName == null || playerName.length == 0 ) {
+        if (playerName == null || playerName.length == 0) {
             throw new Exception("Il faut au minimum un joueur valide");
         }
-        
-        //On ajoute chaque joueur dans le tableau
-        for( String p : playerName) {
-            game.put(p, playerName);
-        }
-        
-        //On lance une partie || -> true
-        GameEnCours = true;
-        
-        //Joueur à itérer + changement de joueur
-        
 
-        
+        //On ajoute chaque joueur dans le tableau
+        for (String p : playerName) {
+            jeu.put(p, new SinglePlayerGame());
+            listejoueur.add(p);
+        }
+
+        //On lance une partie || -> true
+        jeuencours = true;
+
+        //Joueur à itérer + changement de joueur
         return toString();
     }
-    
+
     @Override
     public String lancer(int nombreDeQuillesAbattues) throws Exception {
-        if (!GameEnCours){
+        if (!jeuencours) {
             throw new Exception("La partie est terminée.");
         }
-        
-        GameActuelle.lancer(nombreDeQuillesAbattues);
-        
-        if(GameActuelle.isFinished() || GameActuelle.hasCompletedFrame()) {
-            //Il faut passer au joueur suivant
-            
+
+        jeuactuel.lancer(nombreDeQuillesAbattues);
+
+        if (jeuactuel.isFinished() || jeuactuel.hasCompletedFrame()) {
+            jeuencours = joueurSuivant();
+
         }
         return toString();
     }
 
     @Override
     public int scoreFor(String playerName) throws Exception {
-        SinglePlayerGame g = (SinglePlayerGame) game.get(playerName);
+        SinglePlayerGame g = (SinglePlayerGame) jeu.get(playerName);
         return g.score();
     }
-    
-    public boolean JoueurSuivant() {
-        //il faut passer tous les joueurs
-        if(GameActuelle.isFinished()) {
-            return false;
-        } else {
-            //On reinitialise l'iterateur
+
+    public boolean joueurSuivant() {
+        if (!listejoueur.iterator().hasNext()) {
+            if (jeuactuel.isFinished()) {
+                return false;
+            } else {
+                joueuractuel = listejoueur.get(0);
+            }
         }
-        
-        // iterateur = Joueur en cours 
-        // GameEnCours = game.get(""//joueur itéré);
-	return true;
-        
+        joueuractuel = listejoueur.iterator().next();
+
+        jeuactuel = jeu.get(joueuractuel);
+        return true;
+
     }
-    
+
     @Override
     public String toString() {
         //Si la partie est terminée..
-        if (!GameEnCours) {
+        if (!jeuencours) {
             return "La game est terminée..";
-        //sinon à chaque tour.
-	} else {
-            int nbtour = GameActuelle.getFrameNumber();
-            int nbballe = GameActuelle.getNextBallNumber();
-            return String.format("Prochain tir : joueur %s, tour n° %d, boule n° %d", "Joueur"//Joueur iterer
-                    , nbtour, nbballe);
-	}
+            //sinon à chaque tour.
+        } else {
+            return String.format("Prochain tir : joueur %s, tour n° %d, boule n° %d", joueuractuel, jeuactuel.getFrameNumber(), jeuactuel.getNextBallNumber());
+        }
     }
-    
+
 }
